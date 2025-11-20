fa254dfcfa27f9e0a293049bf081928b24c31dc1
Rakshi2609 <rakshithganjimut@gmail.com>
2025-11-20 12:43:13 +0530

    cors0

--- backend/server.js
+++ backend/server.js
@@ -45,8 +45,9 @@ app.get('/api/config/stripe', (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+// });
+export default app;
